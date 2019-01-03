84c6a07462e8fa4d522cdb511631c99ea2799853
Dilusha100 <dilushadasanayaka@gmail.com>
2019-01-03 17:30:21 +0530

    pdf test

--- routes/request_routes.js
+++ routes/request_routes.js
@@ -304,4 +304,14 @@ function sendRegEmail(refNo,password,name,email)
     });
 }
 
+var fs = require('fs');
+var pdf = require('html-pdf');
+var html = fs.readFileSync('./test/businesscard.html', 'utf8');
+var options = { format: 'Letter' };
+
+pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+  if (err) return console.log(err);
+  console.log(res); // { filename: '/app/businesscard.pdf' }
+});
+
 module.exports = router;
