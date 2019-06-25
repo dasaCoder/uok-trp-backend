57173ab6d0066d080c3dcf81eaef7925bb8f06d9
BurbaCodes <dilushadasanayaka@gmail.com>
2019-06-25 23:01:45 +0530

    db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/uok-trp',
-    //database: 'mongodb://Dilusha100:JfuvsGxn87mQnGU@ds123783.mlab.com:23783/uok-trp',
+    //database: 'mongodb://localhost:27017/uok-trp',
+    database: 'mongodb://Dilusha100:JfuvsGxn87mQnGU@ds123783.mlab.com:23783/uok-trp',
     secret:'my secret'
   }
